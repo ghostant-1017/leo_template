28d8606db26ce0b1b7f5c44ee71bc0382f1f5060
ghostant-1017 <syyanyuhui@qq.com>
2023-12-09 17:55:44 +0800

    update contract owner

--- execute.sh
+++ execute.sh
@@ -1,8 +1,8 @@
 source .env
 
-FUNCTION_NAME=main
-ARG1=
-ARG2=
+FUNCTION_NAME=mint_private
+ARG1=aleo1axv6c4wuah0vaf9mccu2xcv72eup8ncd35v2j0mt3mnkwx04dg9s4r8888
+ARG2=100000000u128
 
 snarkos developer execute \
 --private-key ${PRIVATE_KEY} \
